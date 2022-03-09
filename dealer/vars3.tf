fca3d76e30bd9d0c5165d3bd7f6a3dcc7d09eef1
SaidRupit <said.rupit@gmail.com>
2022-03-09 13:59:29 -0600

    Practica Bucket 2

---
+++ dealer/vars3.tf
@@ -0,0 +1,12 @@
+variable "region" {
+    type = string
+    default = "us-east-2"
+}
+variable "bucket" {
+    type = string
+    default = "marcorupitbucket1"
+}
+
+variable "content" {    
+    type = string
+}   
